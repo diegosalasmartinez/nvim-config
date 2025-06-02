aed2d0f10f8624cbfc31406967582c456fc3aabe
Diego <diesalasmart@gmail.com>
2025-06-02 16:59:58 -0500

    apply format and add bindings for if and try catch blocks

--- lua/diegocentrico/remap.lua
+++ lua/diegocentrico/remap.lua
@@ -43,9 +43,15 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- Add console log line
 vim.keymap.set("n", "<leader>ee", "oconsole.log('[DIEGO]')")
 
+-- Add if block
+vim.keymap.set("n", "<leader>ei", "oif (true) {\n}<Esc>O")
+
+-- Add try/catch block
+vim.keymap.set("n", "<leader>et", "otry {\n} catch (e) {\n}<Esc>kO")
+
 -- Apply lua changes
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+	vim.cmd("so")
 end)
 
 -- Split windows
@@ -58,7 +64,6 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 
-
 local augroup = vim.api.nvim_create_augroup
 local DiegocentricoGroup = augroup("Diegocentrico", {})
 
@@ -66,72 +71,72 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup("HighlightYank", {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+	require("plenary.reload").reload_module(name)
 end
 
 vim.filetype.add({
-    extension = {
-        templ = "templ",
-    },
+	extension = {
+		templ = "templ",
+	},
 })
 
 autocmd("TextYankPost", {
-    group = yank_group,
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = "IncSearch",
-            timeout = 40,
-        })
-    end,
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
 })
 
 autocmd({ "BufWritePre" }, {
-    group = DiegocentricoGroup,
-    pattern = "*",
-    callback = function()
-        -- Ignore work folders
-        if not vim.fn.getcwd():match("/haystack/") then
-            vim.cmd([[ %s/\s\+$//e ]]) -- Remove trailing whitespace
-        end
-    end,
+	group = DiegocentricoGroup,
+	pattern = "*",
+	callback = function()
+		-- Ignore work folders
+		if not vim.fn.getcwd():match("/haystack/") then
+			vim.cmd([[ %s/\s\+$//e ]]) -- Remove trailing whitespace
+		end
+	end,
 })
 
 autocmd("LspAttach", {
-    group = DiegocentricoGroup,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function()
-            vim.lsp.buf.definition()
-        end, opts)
-        vim.keymap.set("n", "K", function()
-            vim.lsp.buf.hover()
-        end, opts)
-        vim.keymap.set("n", "<leader>vws", function()
-            vim.lsp.buf.workspace_symbol()
-        end, opts)
-        vim.keymap.set("n", "<leader>vd", function()
-            vim.diagnostic.open_float()
-        end, opts)
-        vim.keymap.set("n", "<leader>vca", function()
-            vim.lsp.buf.code_action()
-        end, opts)
-        vim.keymap.set("n", "<leader>vrr", function()
-            vim.lsp.buf.references()
-        end, opts)
-        vim.keymap.set("n", "<leader>vrn", function()
-            vim.lsp.buf.rename()
-        end, opts)
-        vim.keymap.set("i", "<C-h>", function()
-            vim.lsp.buf.signature_help()
-        end, opts)
-        vim.keymap.set("n", "[d", function()
-            vim.diagnostic.goto_next()
-        end, opts)
-        vim.keymap.set("n", "]d", function()
-            vim.diagnostic.goto_prev()
-        end, opts)
-    end,
+	group = DiegocentricoGroup,
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		vim.keymap.set("n", "gd", function()
+			vim.lsp.buf.definition()
+		end, opts)
+		vim.keymap.set("n", "K", function()
+			vim.lsp.buf.hover()
+		end, opts)
+		vim.keymap.set("n", "<leader>vws", function()
+			vim.lsp.buf.workspace_symbol()
+		end, opts)
+		vim.keymap.set("n", "<leader>vd", function()
+			vim.diagnostic.open_float()
+		end, opts)
+		vim.keymap.set("n", "<leader>vca", function()
+			vim.lsp.buf.code_action()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrr", function()
+			vim.lsp.buf.references()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrn", function()
+			vim.lsp.buf.rename()
+		end, opts)
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+		vim.keymap.set("n", "[d", function()
+			vim.diagnostic.goto_next()
+		end, opts)
+		vim.keymap.set("n", "]d", function()
+			vim.diagnostic.goto_prev()
+		end, opts)
+	end,
 })
 
 vim.g.netrw_browse_split = 0
